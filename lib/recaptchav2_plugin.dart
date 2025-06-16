652436bdc35cfeb1a90e605ef804a693a21267fa
ryanda <ryandaputra@gmail.com>
2025-06-16 13:26:31 +0700

    update plugin

--- lib/recaptchav2_plugin.dart
+++ lib/recaptchav2_plugin.dart
@@ -1,8 +1,8 @@
 library recaptchav2_plugin;
 
-import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class RecaptchaV2 extends StatefulWidget {
   final String apiKey;
@@ -10,44 +10,81 @@ class RecaptchaV2 extends StatefulWidget {
   final RecaptchaV2Controller controller;
   final bool addCancelButton;
   final String cancelButtonLabel;
-
   final ValueChanged<String> response;
 
   RecaptchaV2({
-    this.apiKey,
-    this.pluginURL = "https://software-incubator.github.io/flutter_recaptcha/",
-    RecaptchaV2Controller controller,
-    this.response,
+    required this.apiKey,
+    required this.pluginURL,
+    RecaptchaV2Controller? controller,
+    required this.response,
     this.addCancelButton = true,
     this.cancelButtonLabel = "CANCEL RECAPTCHA",
   })  : controller = controller ?? RecaptchaV2Controller(),
-        assert(apiKey != null, "Google ReCaptcha API KEY is missing.");
+        assert(apiKey.isNotEmpty, "Google ReCaptcha API KEY is missing.");
 
   @override
-  State<StatefulWidget> createState() => _RecaptchaV2State();
+  State<RecaptchaV2> createState() => _RecaptchaV2State();
 }
 
 class _RecaptchaV2State extends State<RecaptchaV2> {
-  RecaptchaV2Controller controller;
-  WebViewController webViewController;
+  late final RecaptchaV2Controller controller;
+  late final WebViewController webViewController;
+  bool _isWebViewInitialized = false;
 
   void onListen() {
-    if (controller.visible) {
-      if (webViewController != null) {
-        webViewController.clearCache();
-        webViewController.reload();
-      }
+    if (controller.visible && _isWebViewInitialized) {
+      webViewController.clearCache();
+      webViewController.reload();
     }
-    setState(() {
-      controller.visible;
-    });
+    setState(() {});
   }
 
   @override
   void initState() {
+    super.initState();
     controller = widget.controller;
     controller.addListener(onListen);
-    super.initState();
+    _initializeWebViewController();
+  }
+
+  Future<void> _initializeWebViewController() async {
+    // // Setup JavaScript channel
+    // final jsChannel = JavaScriptChannel(
+    //   name: 'RecaptchaFlutterChannel',
+    //   onMessageReceived: (message) => _handleToken(message.message),
+    // );
+
+    // Create and configure WebViewController
+    webViewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      // ..addJavaScriptChannel(jsChannel)
+      ..addJavaScriptChannel(
+        'RecaptchaFlutterChannel',
+        onMessageReceived: (JavaScriptMessage message) {
+          _handleToken(message.message);
+          // ScaffoldMessenger.of(context).showSnackBar(
+          //   SnackBar(content: Text(message.message)),
+          // );
+        },
+      )
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageFinished: (_) {
+            setState(() => _isWebViewInitialized = true);
+          },
+        ),
+      )
+      ..loadRequest(
+        Uri.parse('${widget.pluginURL}?api_key=${widget.apiKey}'),
+      );
+  }
+
+  void _handleToken(String token) {
+    if (token.contains("verify")) {
+      token = token.substring(7);
+    }
+    widget.response(token);
+    controller.hide();
   }
 
   @override
@@ -55,7 +92,7 @@ class _RecaptchaV2State extends State<RecaptchaV2> {
     if (widget.controller != oldWidget.controller) {
       oldWidget.controller.removeListener(onListen);
       controller = widget.controller;
-      controller.removeListener(onListen);
+      controller.addListener(onListen);
     }
     super.didUpdateWidget(oldWidget);
   }
@@ -69,89 +106,70 @@ class _RecaptchaV2State extends State<RecaptchaV2> {
 
   @override
   Widget build(BuildContext context) {
-    return controller.visible
-        ? Stack(
-      children: <Widget>[
-        WebView(
-          initialUrl: "${widget.pluginURL}?api_key=${widget.apiKey}",
-          javascriptMode: JavascriptMode.unrestricted,
-          javascriptChannels: <JavascriptChannel>[
-            JavascriptChannel(
-              name: 'RecaptchaFlutterChannel',
-              onMessageReceived: (JavascriptMessage receiver) {
-                // print(receiver.message);
-                String _token = receiver.message;
-                if (_token.contains("verify")) {
-                  _token = _token.substring(7);
-                }
-                widget.response(_token);
-                controller.hide();
-              },
-            ),
-          ].toSet(),
-          onWebViewCreated: (_controller) {
-            webViewController = _controller;
-          },
-        ),
-        if (widget.addCancelButton)
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: SizedBox(
-              height: 60,
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  Expanded(
-                    child: RaisedButton(
-                      child: Text(widget.cancelButtonLabel),
-                      onPressed: () {
-                        controller.hide();
-                      },
-                    ),
-                  ),
-                ],
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 300),
+      child: controller.visible
+          ? Stack(
+              children: [
+                _buildWebView(),
+                if (widget.addCancelButton) _buildCancelButton(),
+              ],
+            )
+          : const SizedBox.shrink(),
+    );
+  }
+
+  Widget _buildWebView() {
+    return Container(
+      color: Colors.white,
+      child: _isWebViewInitialized
+          ? WebViewWidget(controller: webViewController)
+          : const Center(child: CircularProgressIndicator()),
+    );
+  }
+
+  Widget _buildCancelButton() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: 60,
+        color: Colors.white,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Row(
+          children: [
+            Expanded(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: controller.hide,
+                child: Text(widget.cancelButtonLabel),
               ),
             ),
-          ),
-      ],
-    )
-        : Container();
+          ],
+        ),
+      ),
+    );
   }
 }
 
 class RecaptchaV2Controller extends ChangeNotifier {
-  bool isDisposed = false;
-  List<VoidCallback> _listeners = [];
-
   bool _visible = false;
   bool get visible => _visible;
 
   void show() {
     _visible = true;
-    if (!isDisposed) notifyListeners();
+    notifyListeners();
   }
 
   void hide() {
     _visible = false;
-    if (!isDisposed) notifyListeners();
+    notifyListeners();
   }
 
   @override
   void dispose() {
-    _listeners = [];
-    isDisposed = true;
     super.dispose();
-  }
-
-  @override
-  void addListener(listener) {
-    _listeners.add(listener);
-    super.addListener(listener);
-  }
-
-  @override
-  void removeListener(listener) {
-    _listeners.remove(listener);
-    super.removeListener(listener);
   }
 }
